922b05c5f616ba206e2b6fa4bb6403a68c4627dd
Ivan Semov <Ivan.semov.44@gmail.com>
2026-03-07 12:01:30 +0200

    chore(tooling): add husky, prettier, and commitlint setup

---
+++ commitlint.config.js
@@ -0,0 +1,25 @@
+// https://www.conventionalcommits.org/en/v1.0.0/
+module.exports = {
+  extends: ['@commitlint/config-conventional'],
+  rules: {
+    'type-enum': [
+      2,
+      'always',
+      [
+        'feat',
+        'fix',
+        'docs',
+        'style',
+        'refactor',
+        'perf',
+        'test',
+        'build',
+        'ci',
+        'chore',
+        'revert',
+      ],
+    ],
+    'subject-case': [2, 'never', ['start-case', 'pascal-case', 'upper-case']],
+    'header-max-length': [2, 'always', 100],
+  },
+};
