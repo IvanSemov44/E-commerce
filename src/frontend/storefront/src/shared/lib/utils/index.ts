65406718615a1b9defd41da7ff9b3fc0c7839dcd
Ivan Semov <Ivan.semov.44@gmail.com>
2026-03-23 17:41:58 +0200

    refactor(utils): use named exports for priceFormatter

--- src/frontend/storefront/src/shared/lib/utils/index.ts
+++ src/frontend/storefront/src/shared/lib/utils/index.ts
@@ -3,6 +3,6 @@ export * from './constants';
 export { parseBackendFieldErrors } from './parseBackendFieldErrors';
 export { logger } from './logger';
 export { telemetry } from './telemetry';
-export * from './priceFormatter';
-export { OrderTotals, calculateOrderTotals } from './orderCalculations';
+export { formatPrice, formatPriceLocale } from './priceFormatter';
+export { type OrderTotals, calculateOrderTotals } from './orderCalculations';
 export { isApiError } from './errorUtils';
