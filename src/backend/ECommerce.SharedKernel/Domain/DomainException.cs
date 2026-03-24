8ab42639170e75d89d586ebb5af360cc70e3b4f3
Ivan Semov <Ivan.semov.44@gmail.com>
2026-03-24 16:25:57 +0200

    feat(sharedkernel): Add ECommerce.SharedKernel project

--- src/backend/ECommerce.SharedKernel/Domain/DomainException.cs
+++ src/backend/ECommerce.SharedKernel/Domain/DomainException.cs
@@ -4,12 +4,27 @@ public class DomainException : Exception
 {
     public string Code { get; }
 
+    public DomainException() : base()
+    {
+        Code = "DOMAIN_ERROR";
+    }
+
     public DomainException(string message) : base(message)
     {
         Code = "DOMAIN_ERROR";
     }
 
+    public DomainException(string message, Exception innerException) : base(message, innerException)
+    {
+        Code = "DOMAIN_ERROR";
+    }
+
     public DomainException(string code, string message) : base(message)
+    {
+        Code = code;
+    }
+
+    public DomainException(string code, string message, Exception innerException) : base(message, innerException)
     {
         Code = code;
     }
