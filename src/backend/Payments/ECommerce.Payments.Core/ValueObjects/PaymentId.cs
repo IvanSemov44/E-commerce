20a17386cee91776f965bc7ab2da64bd1d655b60
Ivan Semov <Ivan.semov.44@gmail.com>
2026-05-02 17:05:52 +0300

    refactor(payments): remove Payments.Core project and value objects

--- src/backend/Payments/ECommerce.Payments.Core/ValueObjects/PaymentId.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace ECommerce.Payments.Core.ValueObjects;
-
-public readonly record struct PaymentId(string Value)
-{
-    public static PaymentId New() => new(Guid.NewGuid().ToString("N"));
-
-    public override string ToString() => Value;
-}
