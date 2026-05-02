64fd01e847a68f12a2e81d4992a451e74e4d92bf
Ivan Semov <Ivan.semov.44@gmail.com>
2026-05-02 19:46:02 +0300

    refactor: remove PaymentId value object
    - Payment IDs now use Guid.ToString() directly
    - Simplifies payment identification without wrapper type

--- src/backend/Payments/ECommerce.Payments.Domain/ValueObjects/PaymentId.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace ECommerce.Payments.Domain.ValueObjects;
-
-public readonly record struct PaymentId(string Value)
-{
-    public static PaymentId New() => new(Guid.NewGuid().ToString("N"));
-
-    public override string ToString() => Value;
-}
