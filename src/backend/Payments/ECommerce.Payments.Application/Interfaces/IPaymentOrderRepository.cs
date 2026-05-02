aa0737961587e439ef0b72f16e4c3efa6ffef8d3
Ivan Semov <Ivan.semov.44@gmail.com>
2026-05-02 18:15:18 +0300

    refactor(payments): remove IPaymentOrderRepository and PaymentOrderRepository

--- src/backend/Payments/ECommerce.Payments.Application/Interfaces/IPaymentOrderRepository.cs
+++
@@ -1,9 +0,0 @@
-﻿using ECommerce.SharedKernel.Entities;
-
-namespace ECommerce.Payments.Application.Interfaces;
-
-public interface IPaymentOrderRepository
-{
-    Task<Order?> GetByIdAsync(Guid orderId, bool trackChanges, CancellationToken cancellationToken = default);
-    Task UpdateAsync(Order order, CancellationToken cancellationToken = default);
-}
