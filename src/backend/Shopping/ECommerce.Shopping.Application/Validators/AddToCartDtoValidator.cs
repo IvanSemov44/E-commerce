02f6faa302377cfee825da1f6fb8c157f7474068
Ivan Semov <Ivan.semov.44@gmail.com>
2026-05-14 20:05:52 +0300

    refactor(shopping): move validators to Shopping.Application layer

---
+++ src/backend/Shopping/ECommerce.Shopping.Application/Validators/AddToCartDtoValidator.cs
@@ -0,0 +1,12 @@
+﻿using FluentValidation;
+
+namespace ECommerce.Shopping.Application.Validators;
+
+public class AddToCartDtoValidator : AbstractValidator<DTOs.AddToCartDto>
+{
+    public AddToCartDtoValidator()
+    {
+        RuleFor(x => x.ProductId).NotEmpty().WithMessage("Product ID is required");
+        RuleFor(x => x.Quantity).GreaterThan(0).WithMessage("Quantity must be greater than 0");
+    }
+}
---
+++ src/backend/Shopping/ECommerce.Shopping.Application/Validators/UpdateCartItemDtoValidator.cs
@@ -0,0 +1,11 @@
+﻿using FluentValidation;
+
+namespace ECommerce.Shopping.Application.Validators;
+
+public class UpdateCartItemDtoValidator : AbstractValidator<DTOs.UpdateCartItemDto>
+{
+    public UpdateCartItemDtoValidator()
+    {
+        RuleFor(x => x.Quantity).GreaterThan(0).WithMessage("Quantity must be greater than 0");
+    }
+}
